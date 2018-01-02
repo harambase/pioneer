c7f8055dbfb7cef0efeeafbf71777b8ae35bfcdc
Shilei Lin <lin.shilei@outlook.com>
2018-01-02 18:07:21 +0800

    修改TranscriptList接口

--- src/main/java/com/harambase/pioneer/controller/TranscriptController.java
+++ src/main/java/com/harambase/pioneer/controller/TranscriptController.java
@@ -4,6 +4,7 @@ import com.harambase.common.HaramMessage;
 import com.harambase.pioneer.pojo.Transcript;
 import com.harambase.pioneer.service.TranscriptService;
 import com.harambase.support.util.SessionUtil;
+import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,7 @@ public class TranscriptController {
         this.transcriptService = transcriptService;
     }
 
-    @RequiresPermissions({"admin", "teach"})
+    @RequiresPermissions(value = {"admin", "teach"}, logical = Logical.OR)
     @RequestMapping(value = "/{id}", produces = "application/json", method = RequestMethod.PUT)
     public ResponseEntity update(@PathVariable Integer id, @RequestBody Transcript transcript) {
         transcript.setOperator(SessionUtil.getUserId());
@@ -33,22 +34,39 @@ public class TranscriptController {
         return new ResponseEntity<>(haramMessage, HttpStatus.OK);
     }
 
-    @RequiresPermissions({"admin", "teach", "student", "faculty"})
-    @RequestMapping(value = {"/{studentId}/course", "/{crn}/student"}, produces = "application/json", method = RequestMethod.GET)
-    public ResponseEntity list(@RequestParam(value = "start") Integer start,
-                               @RequestParam(value = "length") Integer length,
-                               @RequestParam(value = "draw") Integer draw,
-                               @RequestParam(value = "search[value]") String search,
-                               @RequestParam(value = "order[0][dir]") String order,
-                               @RequestParam(value = "order[0][column]") String orderCol,
-                               @PathVariable(value = "studentId") String studentId,
-                               @PathVariable(value = "crn") String crn) {
+    @RequiresPermissions(value = {"admin", "teach", "student", "faculty"}, logical = Logical.OR)
+    @RequestMapping(value = "/{studentId}/course", produces = "application/json", method = RequestMethod.GET)
+    public ResponseEntity listByStudentId(@RequestParam(value = "start") Integer start,
+                                          @RequestParam(value = "length") Integer length,
+                                          @RequestParam(value = "draw") Integer draw,
+                                          @RequestParam(value = "search[value]") String search,
+                                          @RequestParam(value = "order[0][dir]") String order,
+                                          @RequestParam(value = "order[0][column]") String orderCol,
+                                          @PathVariable(value = "studentId") String studentId) {
 
-        HaramMessage message = transcriptService.transcriptList(start, length, search, order, orderCol, studentId, crn);
+        HaramMessage message = transcriptService.transcriptList(start, length, search, order, orderCol, studentId, "");
         message.put("draw", draw);
         message.put("recordsTotal", ((LinkedHashMap) message.get("page")).get("totalRows"));
         message.put("recordsFiltered", ((LinkedHashMap) message.get("page")).get("totalRows"));
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
+
+    @RequiresPermissions(value = {"admin", "teach", "student", "faculty"}, logical = Logical.OR)
+    @RequestMapping(value = "/{crn}/student", produces = "application/json", method = RequestMethod.GET)
+    public ResponseEntity listByCRN(@RequestParam(value = "start") Integer start,
+                                    @RequestParam(value = "length") Integer length,
+                                    @RequestParam(value = "draw") Integer draw,
+                                    @RequestParam(value = "search[value]") String search,
+                                    @RequestParam(value = "order[0][dir]") String order,
+                                    @RequestParam(value = "order[0][column]") String orderCol,
+                                    @PathVariable(value = "crn") String crn) {
+
+        HaramMessage message = transcriptService.transcriptList(start, length, search, order, orderCol, "", crn);
+        message.put("draw", draw);
+        message.put("recordsTotal", ((LinkedHashMap) message.get("page")).get("totalRows"));
+        message.put("recordsFiltered", ((LinkedHashMap) message.get("page")).get("totalRows"));
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
 
 }
