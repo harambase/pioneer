a52905cf6ce08e88e60502d88ccaccd07c67d16b
Shilei Lin <lin.shilei@outlook.com>
2018-01-05 16:16:42 +0800

    更新包名

--- src/main/java/com/harambase/pioneer/application/WebController.java
+++ src/main/java/com/harambase/pioneer/application/WebController.java
@@ -11,125 +11,125 @@ public class WebController {
     //不需要login
     @RequestMapping({"/", "/login"})
     public String login() {
-        return "release/login";
+        return "login";
     }
 
     @RequestMapping("/register")
     public String signUp() {
-        return "release/register";
+        return "register";
     }
 
     @RequestMapping("/403")
     public String authError() {
-        return "release/web/403";
+        return "web/403";
     }
 
     @RequestMapping("/404")
     public String pageNotFound() {
-        return "release/web/404";
+        return "web/404";
     }
 
     //只需要用户权限
     @RequestMapping("/index")
     public String welcome() {
-        return "release/index";
+        return "index";
     }
 
     @RequestMapping("/message")
     public String message() {
-        return "release/web/message";
+        return "web/message";
     }
 
     @RequestMapping("/myCourse")
     public String myCourse() {
-        return "release/teach/myCourse";
+        return "teach/myCourse";
     }
 
     //系统设置
     @RequestMapping("/system/user/manage")
     public String user() {
-        return "release/system/user";
+        return "system/user";
     }
 
     @RequestMapping("/system/user/request")
     public String viewRegister() {
-        return "release/system/registration";
+        return "system/registration";
     }
 
     @RequestMapping("/system/role")
     public String viewRole() {
-        return "release/system/role";
+        return "system/role";
     }
 
     //教务系统
     @RequestMapping("/teach/create")
     public String createCourse() {
-        return "release/teach/course";
+        return "teach/course";
     }
 
     @RequestMapping("/teach/view")
     public String viewCourse() {
-        return "release/teach/course";
+        return "teach/course";
     }
 
     @RequestMapping("/teach/edit")
     public String editCourse() {
-        return "release/teach/course";
+        return "teach/course";
     }
 
     @RequestMapping("/teach/choose")
     public String courseChoose() {
-        return "release/teach/courseChoose";
+        return "teach/courseChoose";
     }
 
     @RequestMapping("/teach/pin")
     public String pinInfo() {
-        return "release/teach/pin";
+        return "teach/pin";
     }
 //    @RequestMapping("/teach/advise")
 //    public String fViewAdvising(){
-//        return "release/administration/viewAdvising";
+//        return "administration/viewAdvising";
 //    }
 
     @RequestMapping("/student/course/view")
     public String sViewCourse() {
-        return "release/student/teach/viewCourse";
+        return "student/teach/viewCourse";
     }
 
     @RequestMapping("/student/transcript/view")
     public String sViewTranscript() {
-        return "release/student/teach/viewTranscript";
+        return "student/teach/viewTranscript";
     }
 
     @RequestMapping("/manage/transcript/view")
     public String viewTranscript() {
-        return "release/admin/teach/viewTranscript";
+        return "admin/teach/viewTranscript";
     }
 
     @RequestMapping("/manage/transcript/edit")
     public String editTranscript() {
-        return "release/admin/teach/editTranscript";
+        return "admin/teach/editTranscript";
     }
 
     @RequestMapping("/manage/student/credit")
     public String setCredit() {
-        return "release/admin/teach/setCredit";
+        return "admin/teach/setCredit";
     }
 
     @RequestMapping("/manage/course/request")
     public String viewCourseRequest() {
-        return "release/admin/teach/viewCourseRequest";
+        return "admin/teach/viewCourseRequest";
     }
 
     //后勤管理
     @RequestMapping("/logistic/leave")
     public String viewLeave() {
-        return "release/logistic/viewLeave";
+        return "logistic/viewLeave";
     }
 
     @RequestMapping("/logistic/dorm")
     public String viewDorm() {
-        return "release/logistic/viewDorm";
+        return "logistic/viewDorm";
     }
 
 }
